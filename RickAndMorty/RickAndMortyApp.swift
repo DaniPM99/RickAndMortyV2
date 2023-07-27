c2a1bd40c2d525f1616e2e243f641ee099b43d74
Daniel Parra Martin <dparrama@emeal.nttdata.com>
2023-07-27 20:04:29 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Daniel Parra Martin on 27/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
